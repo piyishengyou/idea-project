fb0edda1500b273efcf0688479999f28a885bcbe
piyisheng <1577261826@qq.com>
2019-06-30 14:20:09 +0800

    添加java到git

---
+++ src/com/itheima/demo/Demo01.java
@@ -0,0 +1,13 @@
+package com.itheima.demo;
+
+public class Demo01 {
+    public static void main(String[] args) {
+
+
+        System.out.println("皮医生");
+        System.out.println("皮医生");
+        System.out.println("皮医生");
+        System.out.println("皮医生");
+        System.out.println("皮医生");
+    }
+}
